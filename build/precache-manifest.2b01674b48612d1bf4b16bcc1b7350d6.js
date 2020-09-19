5c30c7ae41b831e25b9e45a63a8e1f2d428814ae
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-09-19 20:46:08 -0300

    Build

---
+++ build/precache-manifest.2b01674b48612d1bf4b16bcc1b7350d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d732ca00572ace6d721171b1b854e8e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf026bfa13cc57cf3d98",
+    "url": "/static/js/2.311bb8f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.311bb8f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727f6fd54b845fc89d64",
+    "url": "/static/js/main.2767bd14.chunk.js"
+  },
+  {
+    "revision": "2202d88f029176f9c16a",
+    "url": "/static/js/runtime-main.68176411.js"
+  },
+  {
+    "revision": "dc0061637cf53f17277dcc7b8375fc96",
+    "url": "/static/media/ilustrate.dc006163.jpg"
+  },
+  {
+    "revision": "9399ed2a2e373549af72a0be2a874651",
+    "url": "/static/media/this_is_fine.9399ed2a.jpg"
+  }
+]);
